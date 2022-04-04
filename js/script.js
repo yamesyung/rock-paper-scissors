202e7c04bc4b76a976d1a7119208503a7ba4793e
yamesyung <simionut@gmail.com>
2022-04-04 13:46:29 +0300

    added playGame function

--- js/script.js
+++ js/script.js
@@ -5,10 +5,39 @@ function computerPlay() {
     let randomIndex = Math.floor(Math.random() * options.length); 
     let computerOption = options[randomIndex];
 
-    console.log(computerOption)
+    console.log (computerOption)
 
     return computerOption;
 
 }
 
-computerPlay()
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return ("It's a tie!")
+    }
+
+    if (playerSelection == "Rock") {
+        if(computerSelection == "Paper") {
+            return ("You lose! Paper beats Rock.")
+        }
+        else return("You won! Rock beats scissors.")
+    }
+
+    if(playerSelection == "Paper") {
+        if(computerSelection == "Rock") {
+            return ("You won! Paper beats Rock.")
+        }
+        else return("You lose! Scissors beats paper.")
+    }
+
+    if(playerSelection == "Scissors") {
+        if(computerSelection == "Rock") {
+            return ("You lose! Rock beats scissors.")
+        }
+        else return ("You won! Scissors beats paper.")
+    }
+}
+  
+  const playerSelection = "Rock";
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection));
