8036a17492183de9130712a0c9481a9ad9be9ae0
yamesyung <simionut@gmail.com>
2022-04-04 12:31:11 +0300

    added computerPlay function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log("Hello world")
+
+function computerPlay() {
+    let options = ["Rock","Paper", "Scissors"];
+    let randomIndex = Math.floor(Math.random() * options.length); 
+    let computerOption = options[randomIndex];
+
+    console.log(computerOption)
+
+    return computerOption;
+
+}
+
+computerPlay()
