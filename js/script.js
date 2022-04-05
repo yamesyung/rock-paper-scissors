a824b8a0874da070642409c618f9ab706ba5e68c
yamesyung <simionut@gmail.com>
2022-04-05 13:18:28 +0300

    added the score system and comments

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 
-
-function computerPlay() {
-    let options = ["Rock","Paper", "Scissors"];
-    let randomIndex = Math.floor(Math.random() * options.length); 
+//function which picks a random index from an array and returns the computer option
+function computerPlay() {                                               
+    let options = ["Rock","Paper", "Scissors"]
+    let randomIndex = Math.floor(Math.random() * options.length);
     let computerOption = options[randomIndex];
 
     return computerOption;
 
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
+//compares the strings from user input and computer option and returns a proper message for each situation
+function playRound(playerSelection, computerSelection) {                
+    if (playerSelection == computerSelection) {                         
         return ("It's a tie!")
     }
 
@@ -37,12 +38,19 @@ function playRound(playerSelection, computerSelection) {
 
     return("Please add one of the choices")
 }
-  
+
+//standardize the user input, capitalise first letter, however it throws an error when dealing with empty string or the user prompt gets cancelled
 function titleCase(playerSelection){
-    return playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+    if (typeof playerSelection !== "undefined"){
+        return playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()
+    }
+     return("Your entry is empty")
 }
 
+//5 rounds, score gets initialized, display score at the end
 function game(){
+    let playerScore = 0;
+    let computerScore = 0;
     for (let i = 0; i<5; i++) {
         const playerSelection = titleCase(prompt("Add you option:Rock,Paper,Scissors"));
         const computerSelection = computerPlay();
@@ -50,10 +58,16 @@ function game(){
         playRound(playerSelection, computerSelection);
         console.log(playRound(playerSelection,computerSelection))
         
-        
+        if (playRound(playerSelection, computerSelection).includes("won")) {
+            playerScore = playerScore+1;
+
+        }
+        if (playRound(playerSelection, computerSelection).includes("lose")) {
+            computerScore = computerScore+1;
+        }
     }
+    console.log(`The final score is: ` + playerScore + ` : ` + computerScore);
 }
 
   
   game();
-  console.log("The final score is:");
