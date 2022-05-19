9c17d7ae89236229614f9a1d1ec8aa14cf2e074f
yamesyung <simionut@gmail.com>
2022-05-19 13:44:04 +0300

    added working score system

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 
+let playerScore = 0;
+let computerScore = 0;
+
 //function which picks a random index from an array and returns the computer option
 function computerPlay() {                                               
     let options = ["Rock","Paper", "Scissors"]
@@ -13,75 +16,101 @@ const rock = document.querySelector('#rock');
 rock.addEventListener('click' , () => {
     const computerSelection = computerPlay();
     playRound("Rock", computerSelection);
-    console.log(playRound("Rock",computerSelection));
 });
 
 const paper = document.querySelector('#paper');
 paper.addEventListener('click' , () => {
     const computerSelection = computerPlay();
     playRound("Paper", computerSelection);
-    console.log(playRound("Paper",computerSelection));
 });
 
 const scissors = document.querySelector('#scissors');
 scissors.addEventListener('click' , () => {
     const computerSelection = computerPlay();
     playRound("Scissors", computerSelection);
-    console.log(playRound("Scissors",computerSelection));
+    
 });
 
 //compares the strings from user input and computer option and returns a proper message for each situation
-function playRound(playerSelection, computerSelection) {                
-    if (playerSelection == computerSelection) {                         
-        return ("It's a tie!")
+function playRound(playerSelection, computerSelection) {
+    
+    if (playerSelection === computerSelection) {                         
+        divScore.textContent = `It's a tie! 
+        You - ${playerScore}
+        CPU - ${computerScore}`;
+        
     }
 
-    if (playerSelection == "Rock") {
+    else if (playerSelection == "Rock") {
         if(computerSelection == "Paper") {
-            return ("You lose! Paper beats Rock.")
+            ++computerScore;
+            divScore.textContent = `You lose! Paper beats rock. 
+            You - ${playerScore}
+            CPU - ${computerScore}`;
         }
-        else return("You won! Rock beats scissors.")
+        else {
+            ++playerScore;
+            divScore.textContent = `You won! Rock beats scissors.
+            You - ${playerScore}
+            CPU - ${computerScore}`;
+        }
     }
 
-    if(playerSelection == "Paper") {
+    else if(playerSelection == "Paper") {
         if(computerSelection == "Rock") {
-            return ("You won! Paper beats Rock.")
+            ++playerScore;
+            divScore.textContent = `You won! Paper beats rock.
+            You - ${playerScore}
+            CPU - ${computerScore}`;
         }
-        else return("You lose! Scissors beats paper.")
+        else {
+            ++computerScore;
+            divScore.textContent = `You lose! Scissors beats paper.
+            You - ${playerScore}
+            CPU - ${computerScore}`;
+        }
     }
 
-    if(playerSelection == "Scissors") {
+    else if(playerSelection == "Scissors") {
         if(computerSelection == "Rock") {
-            return ("You lose! Rock beats scissors.")
+            ++computerScore;
+            divScore.textContent = `You lose! Rock beats scissors.
+            You - ${playerScore}
+            CPU - ${computerScore}`;
         }
-        else return ("You won! Scissors beats paper.")
+        else {
+            ++playerScore;
+            divScore.textContent = `You won! Scissors beats paper.
+            You - ${playerScore}
+            CPU - ${computerScore}`;
+        }
     }
 
-    return("Please add one of the choices")
+
 }
 
 
 //5 rounds, score gets initialized, display score at the end
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
+// function game(){
+//     let playerScore = 0;
+//     let computerScore = 0;
     
-        //const playerSelection = titleCase(prompt("Add you option:Rock,Paper,Scissors"));
-        const computerSelection = computerPlay();
-        computerPlay();
-        playRound(playerSelection, computerSelection);
-        console.log(playRound(playerSelection,computerSelection))
+//         //const playerSelection = titleCase(prompt("Add you option:Rock,Paper,Scissors"));
+//         const computerSelection = computerPlay();
+//         computerPlay();
+//         playRound(playerSelection, computerSelection);
+//         console.log(playRound(playerSelection,computerSelection))
         
-        if (playRound(playerSelection, computerSelection).includes("won")) {
-            playerScore = playerScore+1;
+//         if (playRound(playerSelection, computerSelection).includes("won")) {
+//             playerScore = playerScore+1;
 
-        }
-        if (playRound(playerSelection, computerSelection).includes("lose")) {
-            computerScore = computerScore+1;
-        }
+//         }
+//         if (playRound(playerSelection, computerSelection).includes("lose")) {
+//             computerScore = computerScore+1;
+//         }
     
-    console.log(`The final score is: ` + playerScore + ` : ` + computerScore);
-}
+//     console.log(`The final score is: ` + playerScore + ` : ` + computerScore);
+// }
 
   
  // game();
